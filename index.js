b3f32c78e763db8746aaa36db043cbfec8a67099
Chris Eickemeyer <Chris.Eickemeyer@gmail.com>
2018-01-22 19:59:05 +0100

    add translations via resources mechanism

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const Alexa = require('alexa-sdk');
 const APP_ID = 'amzn1.ask.skill.6fff2f55-7303-4ca8-9a0e-2c43bf51e255';
 
-
-exports.handler = function(event, context, callback) {
-    const alexa = Alexa.handler(event, context, callback);
-    alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.resources = translations;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask', 'My queen, what would you like to know?');
+        this.emit(':ask', this.t('WELCOME'));
     },
-    'handleQuestion' : function() {
+    'handleQuestion': function () {
         this.response.shouldEndSession(false);
-        this.response.speak('You, my queen!');        
+        this.response.speak(this.t('RESPONSE'));
         this.emit(':responseReady');
     },
-    'AMAZON.StopIntent' : function() {
-        this.emit(':tell', 'Bye bye! And do not forget that you are the best!');
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t['BYE']);
     },
-    'AMAZON.HelpIntent' : function() {
-        this.emit(':tell', 'Now you can ask me a question.');
+    'AMAZON.HelpIntent': function () {
+        this.emit(':tell', this.t['HELP']);
+    }
+};
+
+const translations = {
+    'en': {
+        'translation': {
+            'WELCOME': 'My queen, what would you like to know?',
+            'RESPONSE': 'You, my queen!',
+            'BYE': 'Bye bye! And do not forget that you are the best!',
+            'HELP': 'Now you can ask me a question.'
+        }
+    },
+    'de': {
+        'translation': {
+            'WELCOME': 'Was wünschen Sie zu wissen, eure Hoheit, ?',
+            'RESPONSE': 'Natärlich sie, eure Hoheit!',
+            'BYE': 'Auf Wiedersehen! Und vergessen Sie nicht, Sie sind die Beste und Schönste!',
+            'HELP': 'SIe können mich alles fragen!.'
+        }
     }
 };
